02f9286eaefdc215ee4b9d716e52e109c6708524
Rudranarayan Ratha <138389474+Rudranarayan12@users.noreply.github.com>
2024-07-05 13:57:14 +0530

    solving merge

--- src/JbsFolder/JbsHome/Components/JbsCard/JbsCard.js
+++ src/JbsFolder/JbsHome/Components/JbsCard/JbsCard.js
@@ -8,79 +8,56 @@ export default function JbsCard() {
     <div>
       <Container style={{ marginTop: "2rem" }}>
         <Row>
-          <Col md={12} lg={7} xs={12} className="relative-container345">
-            <div className="text-overlayy123">
-              <div className="choose">Why Choose JBS Interior?</div>
-              <ul className="ullist">
-                <li className="overlist">
-                  Over 300 projects completed, serving 200+ satisfied clients,
-                  bringing extensive experience to each project.
-                </li>
-                <li className="overlist">
-                  We prioritize top-tier materials and trusted partnerships for
-                  consistently superior project outcomes.
-                </li>
-                <li className="overlist">
-                  Transparent communication, attentive listening, and dedication
-                  to delivering promises ensure your satisfaction.
-                </li>
-              </ul>
-            </div>
-            <LazyLoad height={200} offset={100} once>
-              <img
-                src="https://res.cloudinary.com/dtjjk0vwy/image/upload/v1719915883/bmsugma7zsv99zwsdynf.png"
-                className="image4567"
-                loading="lazy"
-                alt="JBS Interior Project"
-              />
-            </LazyLoad>
-          </Col>
-          <Col md={12} lg={5} xs={12}>
-            <LazyLoad height={200} offset={100} once>
-              <img
-                src="https://res.cloudinary.com/dtjjk0vwy/image/upload/v1719905361/pblrx0zwl6vbbxnnpa50.png"
-                className="image14567"
-                loading="lazy"
-                alt="JBS Interior Material"
-              />
-            </LazyLoad>
-          </Col>
-        </Row>
-        <Row className="lastimage-row">
-          <Col md={12} lg={5} xs={12}>
-            <LazyLoad height={200} offset={100} once>
-              <img
-                src="https://res.cloudinary.com/dtjjk0vwy/image/upload/v1719985876/rohpgzcaluxvidncxqff.png"
-                className="image14567"
-                loading="lazy"
-                alt="JBS Interior Partnership"
-              />
-            </LazyLoad>
-          </Col>
-          <Col md={12} lg={7} xs={12} className="final-coll">
-            <div>
-              <LazyLoad height={200} offset={100} once>
-                <img
-                  src="https://res.cloudinary.com/dtjjk0vwy/image/upload/v1719985967/ypr1qa0g6h67vzupbrlx.png"
-                  className="image4567"
-                  loading="lazy"
-                  alt="Contact JBS Interior"
-                />
-              </LazyLoad>
-              <div className="text-overlayy123456">
-                <div className="contact-ussss">Contact us</div>
-                <div className="welove">
-                  We'd love to hear from you and discuss your project. At JBS
-                  Interior, we bring brands to life through cultural insight and
-                  strategic vision. Ready to collaborate?
-                </div>
-                <div className="welove">
-                  Call us at +91-8050141583 or +91-6364926425, or email us at
-                  info@jbsinterior.in.
-                </div>
+          <div className="card-cotainer-background">
+            <div className="card-container-first-image">
+              <h1 className="card-container-first-image-contaent">
+                Why Choose JBS Interior?
+              </h1>
+              <div className="card-container-first-image-uls">
+                <ul>
+                  <li className="mt-2 card-container-first-image-lis">
+                    Over 300 projects completed, serving 200+ satisfied clients,
+                    bringing extensive experience to each project.
+                  </li>
+                  <li className="mt-2 card-container-first-image-lis">
+                    We prioritize top-tier materials and trusted partnerships
+                    for consistently superior project outcomes.
+                  </li>
+                  <li className="mt-2 card-container-first-image-lis">
+                    Transparent communication, attentive listening, and
+                    dedication to delivering promises ensure your satisfaction.
+                  </li>
+                </ul>
               </div>
             </div>
-          </Col>
+            <div className="card-container-second-image"></div>
+          </div>
+        </Row>
+        <Row>
+          <div className="card-cotainer-background">
+            <div className="card-container-third-image"></div>
+            <div className="card-container-fourth-image">
+              <h1 className="card-container-first-image-contaent">
+                Why Choose JBS Interior?
+              </h1>
+              <div className="card-container-first-image-uls">
+                <ul>
+                  <li className="mt-2 card-container-first-image-lis">
+                    Over 300 projects completed, serving 200+ satisfied clients,
+                    bringing extensive experience to each project.
+                  </li>
+                  <li className="mt-2 card-container-first-image-lis">
+                    We prioritize top-tier materials and trusted partnerships
+                    for consistently superior project outcomes.
+                  </li>
+                  <li className="mt-2 card-container-first-image-lis">
+                    Transparent communication, attentive listening, and
+                    dedication to delivering promises ensure your satisfaction.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </Row>
       </Container>
     </div>
