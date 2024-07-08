da14abde6c7bc794634092d9c9c2156f826583ec
Rudranarayan Ratha <138389474+Rudranarayan12@users.noreply.github.com>
2024-07-08 16:49:37 +0530

    somechanges

--- src/GlobalComponent/CardsContainer/SecondSectionSmall.js
+++ src/GlobalComponent/CardsContainer/SecondSectionSmall.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Col } from "react-bootstrap";
+import "./SecondSectionSmall.css";
 
 const SecondSectionSmall = (props) => {
   return (
     <Col sm={5} xs={12} className="secondColomn">
-      <div>
+      <div className="defectSmallCardButton">
         <div className="topcontainedheader">
           {/* <h3>Apartment Complex</h3> */}
           <h3>{props.headername}</h3>
